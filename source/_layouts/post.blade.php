20ac8f7aa6637d424aa71d1850a036da4b88b7b4
Tony Messias <tonyzrp@gmail.com>
2019-01-04 22:09:18 -0300

    Remove card image

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -5,9 +5,6 @@
     <meta property="og:type" content="article" />
     <meta property="og:url" content="{{ $page->getUrl() }}"/>
     <meta property="og:description" content="{{ $page->description }}" />
-    @if ($page->cover_image)
-        <meta property="og:image" content="{{ $page->baseUrl }}{{ $page->cover_image }}" />
-    @endif
 @endpush
 
 @section('body')
