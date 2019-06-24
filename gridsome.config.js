7468961f89cfd3231e6adb1b0e015f3e90d72bee
Tony Messias <tonyzrp@gmail.com>
2019-06-23 22:51:03 -0300

    Fix blog title and link

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: 'Tony Messias'
+  siteDescription: 'Personal blog about Software Engineering and Programming.',
 
   plugins: [
     {
